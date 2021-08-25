865800df94bc3075c7556464c50fbb354e4ea1ca
webcat12345 <rango0986@gmail.com>
2021-08-25 14:47:08 -0700

    action: deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  compress: false,
   env: {
     googleApiKey: 'AIzaSyB5fITwebnKaPmHI-rAGpPNPG7xhIXwbSA',
     api: process.env.API_URL || 'http://127.0.0.1:3005/api',
