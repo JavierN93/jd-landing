d016016ceaf83cdeea6d337a359a5c13df882248
webcat12345 <rango0986@gmail.com>
2021-09-03 10:26:26 -0700

    feat: diy page update

--- pages/services/design/diy.tsx
+++ pages/services/design/diy.tsx
@@ -7,27 +7,42 @@ import { MarketplaceSection } from '../../../components/landing/cta/marketplace-
 import { CustomerType } from '../../../core/data/how-it-works';
 import useGAService from '../../../core/app-services/ga-service';
 import useRequestDesignEstimateService from '../../../core/app-services/request-design-estimate-service';
+import useVideoDialogService from '../../../core/app-services/video-dialog-service';
 
-const step1CarouselImages = [
-  '/assets/images/diy-customers/a1.jpg',
-  '/assets/images/diy-customers/a2.jpg',
-  '/assets/images/diy-customers/a3.jpg',
+const steps = [
+  {
+    title: 'Step 1',
+    description: 'Provide us a photo of the project area',
+    image: 'step-1'
+  },
+  {
+    title: 'Step 2',
+    description: 'Provide us a basic sketch of the project',
+    image: 'step-2'
+  },
+  {
+    title: 'Step 3',
+    description: 'Provide a brief description and provide the materials you would like to use',
+    image: 'step-3'
+  },
 ];
 
-const step2CarouselImages = [
-  '/assets/images/diy-customers/b1.jpg',
-  '/assets/images/diy-customers/b2.jpg',
-  '/assets/images/diy-customers/b3.jpg',
-];
-
-const step3CarouselImages = [
-  '/assets/images/diy-customers/c1.jpg',
-  '/assets/images/diy-customers/c2.jpg',
-  '/assets/images/diy-customers/c3.jpg',
+const receives = [
+  {
+    title: 'Receive',
+    description: 'A Detailed 2D Blueprint',
+    image: 'receive-1'
+  },
+  {
+    title: 'Receive',
+    description: 'A Set Of 3D Renderings',
+    image: 'receive-2'
+  },
 ];
 
 export default function Diy() {
   const requestDesignEstimateService = useRequestDesignEstimateService();
+  const videoDialogService = useVideoDialogService();
   const gaService = useGAService();
   gaService.pageView('https://design.jdlandscaping.net/diy');
   return (
@@ -43,113 +58,80 @@ export default function Diy() {
           </div>
           <div className="absolute bottom-70 w-full">
             <div className="container mx-auto relative z-10">
-              <h1 className="text-30 lg:text-44 text-white text-center lg:text-left max-w-620">
-                Hardscape 3D Design Services
-                For Contractors & DIYers
-              </h1>
+              <h1 className="text-30 lg:text-44 text-white text-center lg:text-left max-w-620">3D Rendering Services</h1>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="container max-w-1220 mx-auto py-70">
+            <h6 className="text-32 text-center mb-40">How It Works</h6>
+            <p className="text-16 text-center max-w-680 mx-auto text-light-400 mb-80">Better articulate your ideas and bring your paper sketch to life in 3 simple steps with J & D Landscaping’s Professional 3D Rendering Services.</p>
+            <div className="text-center md:text-left max-w-1200 mx-auto">
+              {
+                steps.map((step, stepIndex) => (
+                  <div className="grid grid-cols-1 md:grid-cols-2 my-100">
+                    <div className={'flex flex-col justify-center ' + (stepIndex % 2 ? 'order-1 lg:order-2' : 'order-1 lg:order-1')}>
+                      <p className="text-44 text-primary font-medium mb-10">{step.title}</p>
+                      <p className="text-32 text-light-500 max-w-430 mb-20">{step.description}</p>
+                    </div>
+                    <div className={stepIndex % 2 ? 'order-2 lg:order-1' : 'order-2 lg:order-2'}>
+                      <div className={'max-w-500 mt-30 md:mt-0 relative rounded-lg overflow-hidden ' + (stepIndex % 2 ? 'ml-0 mr-auto' : 'mx-auto')}>
+                        <Image className="rounded-lg overflow-hidden cursor-pointer" src={'/assets/images/diy-customers/' + step.image + '.jpg'} placeholder="blur" blurDataURL={'/assets/images/diy-customers/' + step.image + '-blur.png'} width="502" height="316" quality={100} layout="responsive" alt="Carousel Image" />
+                      </div>
+                    </div>
+                  </div>
+                ))
+              }
             </div>
           </div>
         </section>
         <section className="bg-light-50">
-          <div className="container mx-auto py-70">
-            <h6 className="text-24 text-center mb-40">How It Works</h6>
-            <p className="text-16 text-light-400 mb-100 max-w-830 mx-auto px-15">Work online with a Hardscape Architect to plan and design your project and receive a detailed 2D Digital Blueprint, 3D Renderings, and a complete Materials List of your project. It is easy to get started.</p>
+          <div className="container max-w-1230 mx-auto py-70">
+            <h6 className="text-32 text-center mb-80">What you will receive</h6>
             <div className="text-center md:text-left max-w-1200 mx-auto">
-              <div className="grid grid-cols-1 md:grid-cols-2">
-                <div>
-                  <p className="text-44 text-primary font-medium mb-10">1.</p>
-                  <p className="text-16 text-light-500 max-w-430 mb-20">First, submit a basic hand-drawn sketch, a few photos of the project area/house, and measurements of your project. Please don&apos;t worry about the house measurements as we work with GIS Mapping to gather this information.</p>
-                  <p className="text-16 text-light-500 max-w-430">If any other photos or measurements are needed after submission, we will simply reach out to you for this additional information.</p>
-                </div>
-                <div className="pl-0 md:pl-20">
-                  <div className="max-w-500 mt-30 md:mt-0 mr-0 ml-auto relative rounded-lg overflow-hidden">
-                    <Swiper
-                      spaceBetween={20}
-                      slidesPerView={1}
-                      autoplay={{ delay: 4000 }}
-                      pagination={{
-                        el: '.step-1',
-                        type: 'bullets',
-                      }}
-                    >
-                      {step1CarouselImages.map((image: string, imageIndex: number) => (
-                        <SwiperSlide key={imageIndex}>
-                          <div className="rounded-lg shadow-primary overflow-hidden">
-                            <Image className="rounded-lg overflow-hidden cursor-pointer" src={image} width="503" height="316" quality={100} layout="responsive" alt="Carousel Image" />
-                          </div>
-                        </SwiperSlide>
-                      ))}
-                    </Swiper>
-                    <div className="swiper-pagination-slider step-1 absolute w-full h-40 bottom-0 left-0 flex justify-center items-center z-10 bg-secondary bg-opacity-30" />
+              {
+                receives.map((receive, receiveIndex) => (
+                  <div className="grid grid-cols-1 md:grid-cols-2 my-100">
+                    <div className={'flex flex-col justify-center ' + (receiveIndex % 2 ? 'order-1 lg:order-2' : 'order-1 lg:order-1')}>
+                      <p className="text-44 text-primary font-medium mb-10">{receive.title}</p>
+                      <p className="text-32 text-light-500 max-w-430 mb-20">{receive.description}</p>
+                    </div>
+                    <div className={receiveIndex % 2 ? 'order-2 lg:order-1' : 'order-2 lg:order-2'}>
+                      <div className={'max-w-500 mt-30 md:mt-0 relative rounded-lg overflow-hidden ' + (receiveIndex % 2 ? 'ml-0 mr-auto' : 'mx-auto')}>
+                        <Image className="rounded-lg overflow-hidden cursor-pointer" src={'/assets/images/diy-customers/' + receive.image + '.jpg'} placeholder="blur" blurDataURL={'/assets/images/diy-customers/' + receive.image + '-blur.png'} width="502" height="316" quality={100} layout="responsive" alt="Carousel Image" />
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
+                ))
+              }
               <div className="grid grid-cols-1 md:grid-cols-2 my-100">
-                <div>
-                  <div className="max-w-500 mt-30 md:mt-0 relative rounded-lg overflow-hidden">
-                    <Swiper
-                      spaceBetween={20}
-                      slidesPerView={1}
-                      autoplay={{ delay: 4000 }}
-                      pagination={{
-                        el: '.step-2',
-                        type: 'bullets',
-                      }}
-                    >
-                      {step2CarouselImages.map((image: string, imageIndex: number) => (
-                        <SwiperSlide key={imageIndex}>
-                          <div className="rounded-lg shadow-primary overflow-hidden">
-                            <Image className="rounded-lg overflow-hidden cursor-pointer" src={image} width="503" height="316" quality={100} layout="responsive" alt="Carousel Image" />
-                          </div>
-                        </SwiperSlide>
-                      ))}
-                    </Swiper>
-                    <div className="swiper-pagination-slider step-2 absolute w-full h-40 bottom-0 left-0 flex justify-center items-center z-10 bg-secondary bg-opacity-30" />
+                <div className="flex flex-col justify-center">
+                  <p className="text-44 text-primary font-medium mb-10">Receive</p>
+                  <p className="text-32 text-light-500 max-w-430 mb-20">A Set Of 3D Renderings</p>
+                </div>
+                <div className="">
+                  <div className="relative max-w-500 mt-30 md:mt-0 rounded-lg shadow-lg overflow-hidden mx-auto">
+                    <Image src="/assets/images/diy-customers/receive-3.jpg" placeholder="blur" blurDataURL="/assets/images/diy-customers/receive-3-blur.png" width={528} height={344} alt="Complete" layout="responsive" />
+                    <div className="absolute top-1/2 left-1/2 -mt-30 -ml-30 cursor-pointer" onClick={() => {  videoDialogService.play('https://www.youtube.com/embed/y4NQ3LihB4o')}}>
+                      <Image src="/assets/images/icons/youtube-icon.svg" width="60" height="60" quality={100} alt="Joe" />
+                    </div>
                   </div>
-                </div>
-                <div className="pl-0 md:pl-20">
-                  <div className="max-w-500 mr-0 ml-auto">
-                    <p className="text-44 text-primary font-medium mb-10">2.</p>
-                    <p className="text-16 text-light-500 max-w-430 mb-20">Next, one of our expert designers will review your project details and follow up with you to discuss any questions or suggestions based on the initial outline.</p>
-                    <p className="text-16 text-light-500 max-w-430">Once your project is outlined, you&apos;ll be able to choose materials for your project by either telling us what you like, showing us a photo you&apos;ve found, or by looking through our digital showroom of materials.</p>
-                  </div>
-                </div>
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 mb-80">
-                <div>
-                  <p className="text-44 text-primary font-medium mb-10">3.</p>
-                  <p className="text-16 text-light-500 max-w-430">Finally, review your completed 3D drawings, make any minor adjustments needed and receive a complete materials list of everything you need to complete your project based on your plan.</p>
-                </div>
-                <div className="pl-0 md:pl-20">
-                  <div className="max-w-500 mt-30 md:mt-0 mr-0 ml-auto relative rounded-lg overflow-hidden">
-                    <Swiper
-                      spaceBetween={20}
-                      slidesPerView={1}
-                      autoplay={{ delay: 4000 }}
-                      pagination={{
-                        el: '.step-3',
-                        type: 'bullets',
-                      }}
-                    >
-                      {step3CarouselImages.map((image: string, imageIndex: number) => (
-                        <SwiperSlide key={imageIndex}>
-                          <div className="rounded-lg shadow-primary overflow-hidden">
-                            <Image className="rounded-lg overflow-hidden cursor-pointer" src={image} width="503" height="316" quality={100} layout="responsive" alt="Carousel Image" />
-                          </div>
-                        </SwiperSlide>
-                      ))}
-                    </Swiper>
-                    <div className="swiper-pagination-slider step-3 absolute w-full h-40 bottom-0 left-0 flex justify-center items-center z-10 bg-secondary bg-opacity-30" />
+                  <div className="max-w-500 mt-30 md:mt-0 relative rounded-lg overflow-hidden mr-0 ml-auto">
                   </div>
                 </div>
               </div>
             </div>
+          </div>
+        </section>
+        <section className="pt-80 pb-90">
+          <div className="container mx-auto">
             <div className="bg-primary-50 pt-35 pb-25 mb-50 px-10 max-w-1200 mx-auto">
-              <p className="text-24 max-w-725 mx-auto text-center">Receive all this and the guidance of a trusted industry expert for only <span className="text-primary">$399</span></p>
+              <p className="text-24 text-center">Receive all this and the guidance of a trusted industry expert</p>
+              <p className="text-44 text-center">for only <span className="text-primary">$399</span></p>
             </div>
             <div className="flex justify-center">
               <Link href="/submit" passHref>
-                <button className="btn btn-warning btn-lg">Get Started</button>
+                <button className="btn btn-warning btn-lg">Submit Your Project</button>
               </Link>
             </div>
           </div>
